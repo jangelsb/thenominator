22e2a3dae05ea773e1bdba6550fbbca76010198a
buckslice <buckslice@gmail.com>
2016-02-19 21:50:40 -0800

    implemented sklearn pipelines

--- nltkismybae.py
+++ nltkismybae.py
@@ -2,7 +2,11 @@ from nltk import FreqDist
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+
+from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords as nltkstopwords
 from nltk.corpus import wordnet as wn
@@ -32,36 +36,64 @@ def loadReviews():
     pospath = 'dataset/txt_sentoken/pos/'
     negpath = 'dataset/txt_sentoken/neg/'
     posfilenames = listdir(pospath)
-    negfilenames = listdir(negpath);
+    negfilenames = listdir(negpath)
     posreviews = [loadtxtfile(pospath+filename) for filename in posfilenames]
     negreviews = [loadtxtfile(negpath+filename) for filename in negfilenames]
     return posreviews, negreviews
 
-def pipeline_multinomialNB():
+def pipeline_comparison():
     posreviews, negreviews = loadReviews()
     
+    import random
+    import numpy as np
+    #random.seed()
+
+    accuracy = 0.0    
+    percenttrain = 0.8
+    posTrainCount = int(percenttrain*len(posreviews))
+    negTrainCount = int(percenttrain*len(negreviews))
+    
+    for test in range(10):    
+        random.shuffle(posreviews)
+        random.shuffle(negreviews)                
+
+        train_tups = [(r, 1) for r in posreviews[:posTrainCount]] + [(r, 0) for r in negreviews[:negTrainCount]]
+        random.shuffle(train_tups)
+        train_data = [tup[0] for tup in train_tups]
+        Y_train = np.array([tup[1] for tup in train_tups])
+        
+        test_tups = [(r, 1) for r in posreviews[posTrainCount:]] + [(r, 0) for r in negreviews[negTrainCount:]]
+        random.shuffle(test_tups)
+        test_data = [tup[0] for tup in test_tups]
+        actual = np.array([tup[1] for tup in test_tups])
+    
+        #80.70  MULTINOMIAL NAIVE BAYES
+        #79.25  BERNOULLI NAIVE BAYES
+        #83.80  LOGISTIC REGRESSION          <--- winner weeeee
+        #next up try cross validation (split data into 5 chunks and swap around which chunk is test set)
+        #use same shuffled list for each different pipeline
+    
+        #pipe = Pipeline([('vect', CountVectorizer(stop_words=nltkstopwords.words('english'))),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
+        #pipe = Pipeline([('vect', CountVectorizer(stop_words=nltkstopwords.words('english'))),('tfidf', TfidfTransformer()),('clf', BernoulliNB())])
+        pipe = Pipeline([('vect', CountVectorizer(stop_words=nltkstopwords.words('english'))),('tfidf', TfidfTransformer()),('clf', LogisticRegression())])
+        
+        pipe = pipe.fit(train_data, Y_train)
+        prediction = pipe.predict(test_data)
+        
+        correct = 0
+        for i in range(len(actual)):
+            if(actual[i] == prediction[i]):
+                correct+=1
+        
+        print(correct / len(actual))
+        accuracy += correct / len(actual)
     
     
+    print("final accuracy: " + "{:.4f}".format(accuracy / 10.0))
     
-    print(posreviews[0])
-    print(negreviews[1])
-    
-    return
-    
-    train_data = 0  # list of document strings in training data
-    Y_train = 0     # list of ints as positive or negative review
-    test_data = 0   # 
-    pipe = Pipeline([('vect', CountVectorizer(stop_words=nltkstopwords.words('english'))),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
-                     
-    pipe = pipe.fit(train_data, Y_train)
-    return pipe.predict(test_data)
-    
-    
-    
-    
-    
-    
-    
+
+
+
 # filth below    
     
     
