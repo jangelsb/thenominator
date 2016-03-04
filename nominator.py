1d6a4a6a01d1b18284b8dd22a350f69c0c01da24
jangelsb <jangelsb.vcs@gmail.com>
2016-03-04 12:14:30 -0800

    added a grabNFromYears

--- nominator.py
+++ nominator.py
@@ -59,6 +59,46 @@ def grabAllFromYears(fromYear, toYear):
 
 		text_file.close()
 
+def grabNFromYears(fromYear, toYear, n):
+	"""
+	Generates a text file with list of movies from fromYear to toYear but only grabs the first n of each year.
+
+	Returns
+	-------
+	Nothing
+
+	Example
+	-------
+	>>> grabAllFromYears(2009,2015)
+	"""
+	search = tmdb.Discover()
+     
+ 
+	for year in range(fromYear, toYear+1):
+
+		r = search.movie(primary_release_year = year)
+		numOfPages = r['total_pages']
+
+		print("Accessing Year: {}".format(year))
+		print("Number of pages: {}".format(numOfPages))
+
+		#new file per year
+		text_file = open("{}.txt".format(year), "w")
+
+		count = 0
+		for i in range(1, r['total_pages']+1):
+
+			if count > n:
+				break
+
+			print("Page: {}/{}".format(i,numOfPages))
+			r = search.movie(primary_release_year = year, page = i)
+			for a in r['results']:
+				print("{}".format(a['title']), file=text_file)
+				count+=1
+
+		text_file.close()
+
 def grabAllFromYear(year):
 	"""
 	Generates a text file with list of movies from the given year.
